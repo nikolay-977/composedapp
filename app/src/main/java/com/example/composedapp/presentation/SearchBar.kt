b2da27edaa62361d0d25c97c879075a3c47c0850
Nikolay Kostin <nikolay-2708@yandex.ru>
2026-05-05 01:42:25 +0300

    add project

--- app/src/main/java/com/example/composedapp/presentation/SearchBar.kt
+++ app/src/main/java/com/example/composedapp/presentation/SearchBar.kt
@@ -1,5 +1,22 @@
 package com.example.composedapp.presentation
 
-class SearchBar(value: String, onValueChanged: (String) -> Unit) {
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
+@Composable
+fun SearchBar(
+    value: String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChanged,
+        modifier = modifier.fillMaxWidth(),
+        label = { Text( text = "Search...") },
+        singleLine = true,
+    )
 }
