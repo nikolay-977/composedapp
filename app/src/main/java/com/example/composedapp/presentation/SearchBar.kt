817057d67d59dc342671b0cc43d7cd865381d12a
Nikolay Kostin <nikolay-2708@yandex.ru>
2026-05-05 01:42:25 +0300

    add project

---
+++ app/src/main/java/com/example/composedapp/presentation/SearchBar.kt
@@ -0,0 +1,5 @@
+package com.example.composedapp.presentation
+
+class SearchBar(value: String, onValueChanged: (String) -> Unit) {
+
+}
--- app/src/main/java/com/example/composedapp/presentation/TasksScreen.kt
+++ app/src/main/java/com/example/composedapp/presentation/TasksScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.composedapp.data.TaskEntity
+import com.example.composedapp.presentation.SearchBar
 import com.example.composedapp.presentation.TaskState
 import com.example.composedapp.presentation.TasksViewModel
 
